f4654cfc1b04596b054e05333ee903878b55d666
SebastianStork <sebastian.stork@pm.me>
2025-10-19 22:28:02 +0200

    Add linkita theme

--- flake.nix
+++ flake.nix
@@ -1,10 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    linkita = {
+      url = "git+https://codeberg.org/salif/linkita.git";
+      flake = false;
+    };
   };
 
   outputs =
-    { nixpkgs, ... }:
+    {
+      nixpkgs,
+      linkita,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -12,6 +21,11 @@
     {
       devShells.${system}.default = pkgs.mkShell {
         packages = [ pkgs.zola ];
+
+        shellHook = ''
+          mkdir -p themes
+          ln -snf "${linkita}" themes/linkita
+        '';
       };
 
       formatter.${system} = pkgs.nixfmt-tree;
